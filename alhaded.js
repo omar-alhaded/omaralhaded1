13da13eee13ff07d0abfec854e9005189bcb95e7
omar-alhaded <66205421+omar-alhaded@users.noreply.github.com>
2020-08-13 12:57:35 +0300

    Update alhaded.js

--- alhaded.js
+++ alhaded.js
@@ -1,6 +1,19 @@
+$(`<a id="عاصم" href=""><i class="fa fa-refresh fa-spin fa-3x fa-fw" aria-hidden="true"></i></a><img id="asim70"src="https://chatnsh.com/sico/z1ef103tgh10.jpg"style="width:100%"><a id="asim27"target="_blank"type="a"rel="nofollow"title="لتبوكون التصميم ولد الجحش"href="https://www.dookmobile.com"class="label-primary">Programming by Dook Mobile</a></div></div><center>`).insertBefore(".nav-tabs"),$(".nav-tabs").addClass("fl").css("width","100%"),
+$(`<center><div id="asim1">
+<a id="asim7"rel="dofollow" title="القوانين" href="#"style="border-radius: 0 4px 0 0; border-right: 2px solid#f7d322;"class="fl mini  fa fa-bullhorn btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="الاشتراكات" href="#"style="border-right: 1px solid#fff;"class="fl mini  fa fa-star-half-o btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="راديو" href="#"style="border-right: 1px solid#fff;"class="fl mini  fa fa-volume-up btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="زخرفة اسماء" href="#"style="border-right: 1px solid#fff;"class="fl mini  fa fa-fire btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="اتصل بنا" href="#"style="border-right: 1px solid#fff;"class="fl mini fa fa-paper-plane btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="قوانين السوابر" href="#"style="border-right: 1px solid#fff;"class="fl mini fa fa-exclamation-triangle btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="تطبيق الشات" href="#"style="border-right: 1px solid#fff;"class="fl mini fa fa-android btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="اختصارات الشات" href="#"style="border-radius: 6px 0 0 0; border-left: 3px solid#f7d322; border-right: 1px solid#ffffff;"class="fl mini fa fa-tencent-weibo btn btn-primary" target="_blank"></a>
+</div></div><center>`).insertBefore('div#tlogins .lonline.light.break');
+$(`<center><div><marquee direction="right" id="as43" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3"> &nbsp;أهلا وسهلا بكم في شات <font color="#595757" class="asim12">آلــفــجر  </font> ● · ‫أفضل سوبر لهذا الأسبوع · ● <font class="asim11" color="#727A54">&nbsp; هيبة وما بدلت طبعي &nbsp;</font> ● · ‫أفضل زائر لهذا الأسبوع · ● <font class="asim11" color="#ff9900">&nbsp;ســاكــور&nbsp;</font> ● · ‫أفضل زائرة لهذا الأسبوع · ● <font class="asim11" color="#ff00ff">&nbsp; انـآقـة طـآلـبـة&nbsp;</font> ● · مبدع الحائط لهذا الأسبوع · ● <font class="asim11" color="#5CAD9A">&nbsp; امير الخزعلي &nbsp;</font> ● · مبدعة الحائط لهذا الأسبوع · ● <font class="asim11" color="#800000">&nbsp; ريــــاح&nbsp;</font> ● · ‫أفضل تواجد لهذا الأسبوع · ● <font class="asim11" color="#417570">&nbsp; جــوجــايــة&nbsp;</font> ● · مع تمنياتنا للجميع بـ أطيب الاوقات&nbsp;
+</marquee></div><center>`).insertBefore('div#tlogins .lonline.light.break');
 $(`$
 	{
-	'<link rel="stylesheet" href="https://blog.iraqna.net/js/style1.css">'
+	'<link rel="stylesheet" href="https://rawcdn.githack.com/omar-alhaded/omaralhaded1/ad84b137398a1d7eef3aa24652b9c455b74a2045/style1.css">'
 }
 `)['insertBefore']('body');
 $('.ae.label.label-primary.fa.fa-gear')[
@@ -30,7 +43,7 @@ $('.hand.nosel.fl.uzr.border')['css']({
 }
 `), $(`$
 	{
-	'<a href="https://play.google.com/store/apps/details?id=com.md.chatiraq_2" target="_blank" id="as0" class="border">تطبيق الشات</a>'
+	'<a href="#" target="_blank" id="as0" class="border">تطبيق الشات</a>'
 }
 `)['insertBefore'](
   'a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg'
@@ -40,7 +53,7 @@ var nopm = !0
   , nonot = !0;
 $('#d0')['append'](`$
 	{
-	'<label title="اذاعة شكو ماكو"href="#"onclick="$(\'.pnhead\').text($(this).attr(\'title\'));hl($(this),\'primary\');setTimeout(function(){$(\'#mic\').scrollTop(0);},100);$(\'.dpnl\').show()"data-toggle="tab"data-target="#mic"class="asim ae fa label label-primary"><i class="fa fa-volume-up"aria-hidden="true"></i></label>'
+	'<label title="اذاعة الفجر"href="#"onclick="$(\'.pnhead\').text($(this).attr(\'title\'));hl($(this),\'primary\');setTimeout(function(){$(\'#mic\').scrollTop(0);},100);$(\'.dpnl\').show()"data-toggle="tab"data-target="#mic"class="asim ae fa label label-primary"><i class="fa fa-volume-up"aria-hidden="true"></i></label>'
 }
 `);
 $('div#tlogins button.btn.btn-primary')[
@@ -92,7 +105,7 @@ $('textarea#tbox')['on']('paste',
 $('div#l1 button.btn.btn-primary')[
   'bind']('click', function () {
   alert(
-    'اهلا وسهلا بكم في شات شكو ماكو ❤️ سجل عضوية تحصل على 5000 لايك وزخرفة :: طريقة التسجيل اضغط على خيار (تسجيل) اكتب (اكتب اسمك المستعار + كلمة المرور ) وبعدها اضغط دخول'
+    'اهلا وسهلا بكم في شات الفجر ❤️ سجل عضوية تحصل على 5000 لايك وزخرفة :: طريقة التسجيل اضغط على خيار (تسجيل) اكتب (اكتب اسمك المستعار + كلمة المرور ) وبعدها اضغط '
     )
 });
 $(`$
