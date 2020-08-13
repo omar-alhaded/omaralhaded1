1c87190de56ab863d6fbf6a6356c04c99ac4254c
omar-alhaded <66205421+omar-alhaded@users.noreply.github.com>
2020-08-13 12:51:28 +0300

    Create alhaded.js

---
+++ alhaded.js
@@ -0,0 +1,134 @@
+$(`$
+	{
+	'<link rel="stylesheet" href="https://blog.iraqna.net/js/style1.css">'
+}
+`)['insertBefore']('body');
+$('.ae.label.label-primary.fa.fa-gear')[
+  'html'](`$
+	{
+	'⠀'
+}
+`);
+$('label.ae.fa.label.label-primary.fa-commenting-o')[
+  'html'](`$
+	{
+	'⠀'
+}
+`);
+$('.ae.fa.label.label-primary.fa-users')[
+  'html'](`$
+	{
+	'⠀'
+}
+`);
+$('.hand.nosel.fl.uzr.border')['css']({
+  margin: '1px 0'
+  , borderRadius: '0px'
+}), $('.checkbox label')['html'](`$
+	{
+	'<input id="stealth"type="checkbox"value=""><i class="fa fa-eye-slash"aria-hidden="true"style="margin-top: 3px"></i>'
+}
+`), $(`$
+	{
+	'<a href="https://play.google.com/store/apps/details?id=com.md.chatiraq_2" target="_blank" id="as0" class="border">تطبيق الشات</a>'
+}
+`)['insertBefore'](
+  'a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg'
+  ), $('.tc span[class="fa fl"]')[
+  'addClass']('fa-check');
+var nopm = !0
+  , nonot = !0;
+$('#d0')['append'](`$
+	{
+	'<label title="اذاعة شكو ماكو"href="#"onclick="$(\'.pnhead\').text($(this).attr(\'title\'));hl($(this),\'primary\');setTimeout(function(){$(\'#mic\').scrollTop(0);},100);$(\'.dpnl\').show()"data-toggle="tab"data-target="#mic"class="asim ae fa label label-primary"><i class="fa fa-volume-up"aria-hidden="true"></i></label>'
+}
+`);
+$('div#tlogins button.btn.btn-primary')[
+  'click'](function () {
+  setTimeout(function () {
+      refreshonline()
+    }
+    , 0)
+}), setInterval(function () {
+    var _0x4a2fx3 = $('#dpnl');
+    0 < _0x4a2fx3['length'] &&
+      _0x4a2fx3['remove']()
+  }
+  , 0), $('.dpnl')['append'](`$
+	{
+	'<div id="mic"style="height: 100%;width:100%;"class="break light tab-pane border"><center></div><center></div>'
+}
+`), $(
+  'div#tlogins button.btn.btn-primary'
+  )['click'](function () {
+  var _0x4a2fx4 = setInterval(
+    function () {
+      var _0x4a2fx5 = $('.pmsgc')[
+        'length'];
+      if (_0x4a2fx5 > 0) {
+        $(`$
+				{
+				'<center><div></div><center><iframe frameborder="0"id="asim29"src="https://blog.iraqna.net/embed"></iframe><iframe frameborder="0" id="asim30" src=""></iframe></a>\x0D\x0A<a id="asim19"rel="dofollow" title="اتصل بالاذاعه" href="https://discord.gg/eQRwjt5"class="fl mini fa fa-tencent-weibo btn btn-primary" target="_blank"></a></div><center><div><textarea class="as2020" style="height:146px;overflow:hidden">\x0D\x0A⠀ ⠀ ⠀ ⠀ ⠀ ⠀ ⠀ ⠀ \x0D\x0A♔ ★  ║ 彡  ♫ • ❥ ✿  ⌯ ✘ ●┊♡♰❰ ❱ ᵛ͢ᵎᵖ  ₎™ 🦅🌹💀☠️👑🌝✨💥🔥🍂☂ ❥ ‏˛⁽ ツ‏⠀ ᵀᴹ )"̯ء \x0D\x0Aڪـٰٖـٖ ـٰـٰٰـٰٰة ـٰٰٰٰ۪ٖ۫ـٰہـةﺔﺔ ،آ‘إْــِْـﮧٰٖـــــِْﮧٰٖــ ۽ۧ۽ۧ ــٰٰـٰـٰٰ ـٰٰـٰـٰٰـﮯ̷͡ـــ ـــٰۦـٰٖـٰـٰہٰٖءـٰـٰۦــ ــ♯ــٰـٰـٰۦـٰٰٰٖٖ\x0D\x0Aqતۦۦ᎗ۦۦ᎗බລܭɹ̇൧ρᘓ۶ᒧხbl͠ \x0D\x0A</textarea><iframe frameborder="0" id="asim222" src="https://blog.iraqna.net/coolnames/index/index/www.chatiraqiaa.com/index.html" style="height:280px"></iframe></div>'
+			}
+			`)['appendTo']('#mic');
+        clearInterval(_0x4a2fx4)
+      } else {
+        console['log'](_0x4a2fx5)
+      }
+    }
+    , 2000)
+});
+$('textarea#tbox')['bind'](
+  'contextmenu'
+  , function (_0x4a2fx6) {
+    _0x4a2fx6['preventDefault']()
+  }
+  , false);
+$('textarea#tbox')['on']('paste',
+  function (_0x4a2fx6) {
+    _0x4a2fx6['preventDefault']()
+  }
+);
+$('div#l1 button.btn.btn-primary')[
+  'bind']('click', function () {
+  alert(
+    'اهلا وسهلا بكم في شات شكو ماكو ❤️ سجل عضوية تحصل على 5000 لايك وزخرفة :: طريقة التسجيل اضغط على خيار (تسجيل) اكتب (اكتب اسمك المستعار + كلمة المرور ) وبعدها اضغط دخول'
+    )
+});
+$(`$
+	{
+	'<style>.hand.corner{background-color:#000!important;border:2px solid #c12e2a!important;color:#f0ad4e;border-radius:0;text-align:center;border-style:solid;border-right:7px solid!important;border-left:7px solid!important;border-color:#2ea1ad;border-left-style:double!important;border-right-style:double!important}div.break.fl::before{content:" اخي الزائر اختي الزائرة اذا واجهت مشكله في دخول الشات يرجى مراسلتنا من صفحة اتصل بنا 📨 .. ";color:#0f0!important;font-family:\'jazeera\',FontAwesome;font-size:16px}.break.fl{text-align:center;padding:5px!important;font-family:\'jazeera\',FontAwesome;color:red;-webkit-animation:mymove .5s infinite;animation:mymove .5s infinite}</style>'
+}
+`)['insertBefore'](
+  'div#tlogins .lonline.light.break');
+$('div#tlogins button.btn.btn-primary')[
+  'click'](function () {
+  var _0x4a2fx3 = setInterval(
+    function () {
+      var _0x4a2fx7 = $('.pmsgc')[
+        'length'];
+      0 < _0x4a2fx7 ? ($(`$
+			{
+			'<iframe src="https://blog.iraqna.net/embed/d2.php" width="26px" height="26px" scrolling="no" frameborder="no" marginheight="0" marginwidth="0" style="float: left;width: 26px;margin-top: 0px;height: 26px;position: absolute;left: 65px;"></iframe>'
+		}
+		`)['insertAfter'](
+        'button.fa.fa-sign-out.fl.btn.btn-primary'
+        ), clearInterval(
+        _0x4a2fx3)) : console[
+        'log'](_0x4a2fx7)
+    }
+    , 1)
+}), $(
+  'div#tlogins button.btn.btn-primary'
+  )['click'](function () {
+  setTimeout(function () {
+      refreshonline()
+    }
+    , 0)
+});
+$(`$
+	{
+	'<img tabindex="0" class="smoke fl " style="padding:5px;width: 55px;margin-top: -6px;margin-left: -17px;margin-right: -8px;"data-original-title="" title="">'
+}
+`)['insertAfter'](
+  'img.fl.nosel.emobox')
